6e84af4be88afb7c26d6c56363844c841660fe5c
Andrew Allbright <andrewa@angieslist.com>
2015-09-14 02:15:13 -0400

    Updated template and readme.

--- README.md
+++ README.md
@@ -10,3 +10,26 @@ python main.py -d 32kKjs2 --tmp ./tmp_directory
 
 ```
 
+### Sample output
+```
+➜  program-template git:(master) ✗ python main.py -d klajsf --tmp alkjsdfklj
+---- - - - Program start... - - - ----
+-- Program Description: Description of program  --
+
+
+## Arguments
+
+- - Argument registered with name of: example, help text of: default help text
+
+
+### Optional arguments
+
+- - Optional argument registered with name of: --tmp, help text of: default help text
+Program primary function go
+....You passed in klajsf for -d
+....You passed in alkjsdfklj for --tmp
+
+
+---- - - - Program complete! - - - ----
+```
+
--- main.py
+++ main.py
@@ -16,37 +16,48 @@ def Arg(name,
     'help': str(help)
   }
 
+def log(logText):
+  # Easy to add volatile or more permanent solution later.
+  print(logText)
+
 # configuration
 config = {
   'description': 'Description of program',
   'help': 'usage: python main.py -d directory --tmp temporary directory in case program failure',
-  'args': {
-    'example': Arg('example', terminalVariableName='-d')
-  },
-  'optionalArgs': {
-    'optionalExample': Arg('optionalExample', terminalVariableName='--tmp', defaultValue='/tmp')
-  }
+  'args': [
+    Arg('example', terminalVariableName='-d')
+  ],
+  'optionalArgs': [
+    Arg('optionalExample', terminalVariableName='--tmp', defaultValue='/tmp')
+  ]
 }
 
 # Get work done:
 if __name__ == "__main__":
-  print('# Program Description: %s #' % config['description'])
+  log('---- - - - Program start... - - - ----')
+  log('-- Program Description: %s  --' % config['description'])
+  # Set up args -- fill in via config above
   parser = argparse.ArgumentParser(description=config['description'])
 
-  print('\n\n##Arguments')
-  for name, arg in config['args'].iteritems():
-    print('- - Argument registered with name of: %s, help text of: %s' % (arg['terminalVariableName'], arg['help']))
+  log('\n\n## Arguments\n')
+  for arg in config['args']:
+    log('- - Argument registered with name of: %s, help text of: %s' % (arg['name'], arg['help']))
     parser.add_argument(arg['terminalVariableName'], help=arg['help'])
 
-  print('\n###Optional arguments')
-  for name, arg in config['optionalArgs'].iteritems():
-    print('- - Optional argument registered with name of: %s, help text of: %s' % (arg['terminalVariableName'], arg['help']))
-    parser.add_argument(arg['terminalVariableName'] or arg['name'], nargs='?', help=arg['help'])
+  log('\n\n### Optional arguments\n')
+  for arg in config['optionalArgs']:
+    log('- - Optional argument registered with name of: %s, help text of: %s' % (arg['terminalVariableName'], arg['help']))
+    parser.add_argument(arg['terminalVariableName'], nargs='?', help=arg['help'])
 
-  # Parse arguments if any
   args = parser.parse_args()
-  pprint(dir(args))
-  pprint(args)
 
-  print('\n\nProgram complete!')
+  print('Program primary function go')
+  # Program function logic
+
+  print('....You passed in %s for -d' % (args.d))
+  if args.tmp:
+    print('....You passed in %s for --tmp' %(args.tmp))
+
+  # Program function logic end
+  print('\n\n---- - - - Program complete! - - - ----\n\n\n')
 
